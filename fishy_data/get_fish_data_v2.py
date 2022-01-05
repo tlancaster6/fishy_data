3adea82ef19ead00a2f9dc9e2f7387026a402c1b
tlancaster <tlancaster6@gatech.edu>
2022-01-05 15:35:11 -0500

    prepping v2 script for scale testing

--- fishy_data/get_fish_data_v2.py
+++ fishy_data/get_fish_data_v2.py
@@ -1,10 +1,10 @@
 import cv2
 import subprocess as sp
-import pandas as pd
 from pathlib import Path, PurePath, PurePosixPath
 from datetime import datetime as dt
 import shutil
 import os
+import argparse
 
 class VideoParser:
 
@@ -13,7 +13,16 @@ class VideoParser:
         self.fm = FileManager(timestep, subset)
         self.fm.mkdir_local(self.fm.data_dir)
 
+    def execute(self):
+        # execute all steps in video parsing
+        pids = self.get_pids()
+        for pid in pids:
+            self.parse_project(pid)
+        self.upload_imgs()
+
     def get_pids(self):
+        # gather all project ID's that meet the subset, min_year, and min_month conditions
+        print('determining valid projects')
         all_pids = self.fm.ls_cloud(self.fm.data_dir, dirs_only=True)
 
         if self.subset == 'rock':
@@ -28,46 +37,53 @@ class VideoParser:
             if (self.subset == 'all') or (any(s in pid for s in search_strings)):
                 year, month = self.check_creation_date(pid)
                 if (year >= self.min_month) and (month >= self.min_month):
-                    valid_pids.append(pid)
+                    valid_pids.append(pid.strip('/'))
+        print(f'{len(valid_pids)} valid projects located')
         return valid_pids
 
     def check_creation_date(self, pid):
+        # check a project's creation date robustly using the logfile
         rpath = self.fm.data_dir / pid / 'logfile.txt'
+        self.fm.download(rpath)
         lpath = (self.fm.local_root / rpath)
         if not lpath.exists():
-            return
+            return 0, 0
         with open(lpath) as f:
             for line in f:
                 if line.startswith('MasterRecordInitialStart:'):
-                    t = line.split(': ')[-1]
+                    t = line.split(': ')[-1].strip()
+                    print(t)
                     t = dt.strptime(t, '%Y-%m-%d %H:%M:%S.%f')
         self.fm.local_delete(rpath)
         return t.year, t.month
 
     def parse_project(self, pid):
+        # parse all videos from a given project (pid) into images
+        print(f'parsing videos for {pid}')
         vid_dir = self.fm.data_dir / pid / 'Videos'
-        vid_names = [n for n in self.fm.ls_cloud(vid_dir) if '.mp4' in n]
+        vid_names = [n for n in self.fm.ls_cloud(vid_dir) if (('.mp4' in n) or ('.h264' in n))]
         for vname in vid_names:
             vid_path = vid_dir / vname
             self.fm.download(vid_path)
-            cap = cv2.VideoCapture(self.fm.local_root / vid_path)
+            cap = cv2.VideoCapture(str(self.fm.local_root / vid_path))
             fps = int(round(cap.get(cv2.CAP_PROP_FPS)))
             frame_step = int(self.timestep * 60 * fps)
             count = 0
             while cap.isOpened():
                 ret, frame = cap.read()
                 if ret:
-                    img_name = self.file_namer(pid, vname, count, fps)
-                    img_path = self.fm.local_root / self.fm.data_dir / img_name
-                    cv2.imwrite(img_path, frame)
+                    img_name = self.generate_filename(pid, vname, count, fps)
+                    img_path = self.fm.local_root / self.fm.results_dir / img_name
+                    cv2.imwrite(str(img_path), frame)
+                    print(f'writing {img_path}')
                     count += frame_step
                     cap.set(cv2.CAP_PROP_POS_FRAMES, count)
                 else:
                     cap.release()
                     break
+            self.fm.local_delete(vid_path)
 
-
-    def file_namer(self, projectID, v_file, frame_count, fps):
+    def generate_filename(self, projectID, v_file, frame_count, fps):
         """
         -creates a filename in the format <parent-projectID>_<parent-video-name>_<step(min)>_<frame>_<hr-min-sec.ms>.jpg
         :return: {str} <filename>
@@ -80,27 +96,33 @@ class VideoParser:
         filename = '{}_{}_{}_{}_{}.jpg'.format(projectID, v_file.split('.')[0], self.timestep, frame_count, time)
         return filename
 
+    def upload_imgs(self):
+        # upload the results directory
+        self.fm.upload(self.fm.results_dir)
+
 class FileManager:
 
     def __init__(self, timestep, subset):
+        print('initializing file manager')
         self.local_root = Path.home() / 'BioSci-McGrath' / 'Apps' / 'CichlidPiData'
         self.cloud_root = PurePosixPath('cichlidVideo:') / 'BioSci-McGrath' / 'Apps' / 'CichlidPiData'
         self.data_dir = PurePath('__ProjectData')
         self.results_dir = PurePath(f'__TrainingData/CichlidDetection/Training2021/Images_{timestep}/{subset}')
         self.logfile = self.results_dir / 'parsing.log'
 
+        self.mkdir_local(self.data_dir)
+        self.mkdir_local(self.results_dir)
+
     def mkdir_local(self, rpath):
         p = self.local_root / rpath
         if not p.exists():
-            p.mkdir()
+            p.mkdir(parents=True)
 
     def download(self, rpath):
-        print(f'downloading {rpath}')
-        sp.run(['rclone', 'copy', self.cloud_root / rpath, self.local_root / rpath])
+        sp.run(['rclone', 'copyto', self.cloud_root / rpath, self.local_root / rpath])
 
     def upload(self, rpath):
-        print(f'uploading {rpath}')
-        sp.run(['rclone', 'copy', self.local_root / rpath, self.cloud_root / rpath])
+        sp.run(['rclone', 'copyto', self.local_root / rpath, self.cloud_root / rpath])
 
     def ls_cloud(self, rpath, dirs_only=False):
         if dirs_only:
@@ -118,6 +140,13 @@ class FileManager:
             shutil.rmtree(p)
 
 
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s', '--subset', type=str, default='all',
+                        help='Enter desired analysis subset (such as rock, sand, or all), or a custom search string')
+    parser.add_argument('-t', '--timestep', type=int, default=15,
+                        help='Enter the desired time interval (in minutes) between captured frames')
+    args = parser.parse_args()
 
-
-
+    vp = VideoParser(timestep=args.timestep, subset=args.subset)
+    vp.execute()
