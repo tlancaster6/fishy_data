0683fedc0a169019d80c044f81b0502a801af946
tlancaster <tlancaster6@gatech.edu>
2022-01-10 10:25:59 -0500

    path error fix

--- fishy_data/get_fish_data_v2.py
+++ fishy_data/get_fish_data_v2.py
@@ -110,7 +110,8 @@ class VideoParser:
             if '.h264' in vname:
                 vname = vname.replace('.h264', '.mp4')
                 new_path = vid_dir / vname
-                command = ['ffmpeg', '-r', '30', '-i', vid_path, '-threads', '1', '-c:v', 'copy', '-r', '30', new_path]
+                command = ['ffmpeg', '-r', '30', '-i', self.fm.local_root / vid_path, '-threads', '1', '-c:v', 'copy',
+                           '-r', '30', self.fm.local_root / new_path]
                 sp.run(command)
                 self.fm.local_delete(vid_path)
                 vid_path = new_path
